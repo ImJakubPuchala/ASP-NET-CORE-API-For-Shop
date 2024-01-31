2e5b1b5e52511c26bd30727655e1c3ca7f759c9d
ImJakubPuchala <86433812+ImJakubPuchala@users.noreply.github.com>
2024-01-31 09:23:22 +0100

    new table

--- database.sql
+++ database.sql
@@ -14,3 +14,25 @@ CREATE TABLE Prices
     EndDate DATE NULL,
     CONSTRAINT FK_Prices_Products FOREIGN KEY (ProductID) REFERENCES Products (ProductID)
 );
+
+CREATE TABLE Sales (
+    SaleId INT IDENTITY PRIMARY KEY,
+    ProductId INT NOT NULL,
+    QuantitySold INT NOT NULL,
+    FOREIGN KEY (ProductId) REFERENCES Products(ProductId)
+);
+
+CREATE TABLE Reviews (
+    ReviewId INT IDENTITY PRIMARY KEY,
+    ProductId INT NOT NULL,
+    Rating FLOAT NOT NULL,
+    FOREIGN KEY (ProductId) REFERENCES Products(ProductId)
+);
+
+CREATE TABLE Warehouse (
+    WarehouseId INT IDENTITY PRIMARY KEY,
+    ProductId INT NOT NULL,
+    Quantity INT NOT NULL,
+    WarehouseNumber NVARCHAR(50) NOT NULL,
+    FOREIGN KEY (ProductId) REFERENCES Products(ProductId)
+);
