2e5dbb567b4e0b2ae647374208b801bcbb77315c
Jakub Puchala <86433812+ImJakubPuchala@users.noreply.github.com>
2024-02-15 22:30:12 +0100

    middleware

---
+++ Middleware/LoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace ASP_NET_CORE_API_For_Shop.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"[Request] {context.Request.Method} {context.Request.Path}");
+
+            await _next(context);
+
+            Console.WriteLine($"[Response] {context.Response.StatusCode}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using ASP_NET_CORE_API_For_Shop.Middleware;
 using ASP_NET_CORE_API_For_Shop.Model;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,11 +26,14 @@ builder.Services.AddDbContext<ShopContext>(options =>
 
 var app = builder.Build();
 
+app.UseMiddleware<LoggingMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowAll");
 }
 
 app.UseHttpsRedirection();
