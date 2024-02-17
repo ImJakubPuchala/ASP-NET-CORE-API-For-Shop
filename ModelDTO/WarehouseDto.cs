1f8fd717eacd34183e507151357a973b02f2dba6
Jakub Puchala <86433812+ImJakubPuchala@users.noreply.github.com>
2024-02-17 21:30:21 +0100

    DTO

---
+++ ModelDTO/WarehouseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ASP_NET_CORE_API_For_Shop.ModelDTO;
+
+
+public class WarehouseDto
+{
+    public string EANCode { get; set; }
+    public int Quantity { get; set; }
+    public string WarehouseNumber { get; set; }
+}
+
+public class UpdateWarehouseNumberDto
+{
+    public string NewWarehouseNumber { get; set; }
+}
